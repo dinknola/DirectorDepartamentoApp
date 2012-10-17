6343aa34db0f45d07e59549576af4f1aeeea0472
Daniel M <danielocuras@hotmail.com>
2012-10-17 01:42:20 -0500

    Classes
    Creation

---
+++ DirectorDepartamento/src/Control/ConsultarInformacion.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Control;
+import Entidad.*;
+
+/**
+ *
+ * @author Dan
+ */
+public class ConsultarInformacion {
+    
+    
+    public ConsultarInformacion(){
+     
+    }
+
+
+
+
+}
---
+++ README.txt
@@ -0,0 +1,1 @@
+Download and Add to Netbeans
